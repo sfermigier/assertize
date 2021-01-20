4fc9e90ea4ed85d94bb69bbfabd7eabf88e5aaf5
Stefane Fermigier <sf@fermigier.com>
2021-01-20 14:11:03 +0100

    Initial import.

---
+++ src/assertize.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+import sys
+
+import libcst as cst
+import libcst.matchers as m
+# from devtools import debug
+
+
+class TestTransformer(m.MatcherDecoratableTransformer):
+    def leave_Call(
+        self, original_node: cst.Call, updated_node: cst.Call
+    ) -> cst.CSTNode:
+        call = original_node
+        func = call.func
+        args = call.args
+
+        matchers = [
+            [
+                m.Call(
+                    func=m.Attribute(value=m.Name("self"), attr=m.Name("assertEqual"))
+                ),
+                2,
+                lambda: cst.Assert(
+                    cst.Equal(cst.Arg(args[0].value), cst.Arg(args[1].value))
+                ),
+            ],
+            [
+                m.Call(
+                    func=m.Attribute(
+                        value=m.Name("self"), attr=m.Name("assertNotEqual")
+                    )
+                ),
+                2,
+                lambda: cst.Assert(
+                    cst.Not(cst.Equal(cst.Arg(args[0].value), cst.Arg(args[1].value)))
+                ),
+            ],
+            [
+                m.Call(
+                    func=m.Attribute(value=m.Name("self"), attr=m.Name("assertTrue"))
+                ),
+                1,
+                lambda: cst.Assert(cst.Arg(args[0].value)),
+            ],
+            [
+                m.Call(
+                    func=m.Attribute(value=m.Name("self"), attr=m.Name("assertIsNone"))
+                ),
+                1,
+                lambda: cst.Assert(
+                    cst.Comparison(
+                        args[0].value,
+                        [cst.ComparisonTarget(cst.Is(), cst.Name("None"))],
+                    )
+                ),
+            ],
+            [
+                m.Call(
+                    func=m.Attribute(
+                        value=m.Name("self"), attr=m.Name("assertIsNotNone")
+                    )
+                ),
+                1,
+                lambda: cst.Assert(
+                    cst.Comparison(
+                        args[0].value,
+                        [
+                            cst.ComparisonTarget(
+                                cst.Is(), cst.parse_expression("not None")
+                            )
+                        ],
+                    )
+                ),
+            ],
+        ]
+
+        for matcher, arity, replacement in matchers:
+            if m.matches(call, matcher):
+                if len(args) == arity:
+                    # debug(replacement())
+                    return replacement()
+
+        return updated_node
+
+
+def rewrite_module(code: str) -> str:
+    tree = cst.parse_module(code)
+
+    transformer = TestTransformer()
+    modified_tree = tree.visit(transformer)
+
+    return modified_tree.code
+
+
+def main():
+    for filename in sys.argv[1:]:
+        print(filename)
+        with open(filename) as fd:
+            old_code = fd.read()
+            new_code = rewrite_module(old_code)
+
+        with open(filename, "w") as fd:
+            fd.write(new_code)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_all.py
@@ -0,0 +1,19 @@
+import assertize
+
+
+def test_assertequal():
+    src = """self.assertEqual(a, b)"""
+    res = assertize.rewrite_module(src)
+    assert res == "assert a==b"
+
+
+def test_assertisnone():
+    src = """self.assertIsNone(xxx)"""
+    res = assertize.rewrite_module(src)
+    assert res == "assert xxx is None"
+
+
+def test_assertisnotnone():
+    src = """self.assertIsNotNone(xxx)"""
+    res = assertize.rewrite_module(src)
+    assert res == "assert xxx is not None"
